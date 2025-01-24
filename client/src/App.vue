c1a59e2e70493552ba67f9450ba81fc228aa2791
Ryan Streur <ryan@ryanstreur.com>
2025-01-24 12:31:25 -0800

    add vue router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <ul>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/about">About</RouterLink>
+      </li>
+    </ul>
+  </nav>
+  <main>
+    <RouterView></RouterView>
+  </main>
 </template>
 
 <style scoped>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createMemoryHistory, createRouter } from "vue-router";
+
+import HomeView from "./routes/HomeView.vue";
+import AboutView from "./routes/AboutView.vue";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/about", component: AboutView },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
